c1ec7c988d6b4a1032e9c16fc956a63452e7b448
srhbnnstr <srhbnnstr@gmail.com>
2016-05-26 21:40:25 -0700

    added relationships between models and added erd gem

--- app/models/doodle.rb
+++ app/models/doodle.rb
@@ -1,2 +1,4 @@
 class Doodle < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :list
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,5 @@
 class List < ActiveRecord::Base
+  belongs_to :user
+  has_many :todos
+  has_many :doodles
 end
