2431b90d33b78658f8c7fd1c517048d4b38e81b1
srhbnnstr <srhbnnstr@gmail.com>
2016-05-30 15:56:25 -0700

    login, sign up, index page are styled. Need to add about sectin on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.photo == ""
-      @user.photo = " "
+      @user.photo = "pencil-icon"
     end
     @user.save
     login(@user)
